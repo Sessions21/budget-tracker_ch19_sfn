674062b83cfb58b8e9b998fc1963f4958a498eff
Scott Nichols <s21nichols@hotmail.com>
2022-03-12 13:34:18 -0700

    tested offline functionality and corrected some code

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,11 +2,9 @@ let db;
 
 const request = indexedDB.open('budget', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+
 request.onupgradeneeded = function(event) {
-  // save a reference to the database 
   const db = event.target.result;
-  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
   db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
@@ -24,13 +22,11 @@ request.onerror = function(event) {
   console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
-function saveRecord(record) {
-  // open a new transaction with the database with read and write permissions 
-  const transaction = db.transaction(['budget'], 'readwrite');
 
-  // access the object store for `budget`
-  const budgetObjectStore = transaction.objectStore('budget');
+function saveRecord(record) {
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  const budgetObjectStore = transaction.objectStore('new_transaction');
 
   budgetObjectStore.add(record);
 }
